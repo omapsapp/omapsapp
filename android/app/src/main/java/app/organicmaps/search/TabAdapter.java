ffcb6c315b2153ba38727a5b2f182749b4a8453b
varad64 <varad23711@users.noreply.github.com>
2024-10-06 16:12:40 +0530

    #8773 - Use SharedPreferences to restore the last selected search tab on app restart.
    Signed-off-by: varad64 <varad23711@users.noreply.github.com>

--- android/app/src/main/java/app/organicmaps/search/TabAdapter.java
+++ android/app/src/main/java/app/organicmaps/search/TabAdapter.java
@@ -1,6 +1,7 @@
 package app.organicmaps.search;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.SparseArray;
 import android.view.View;
 
@@ -11,6 +12,8 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+
+import app.organicmaps.MwmApplication;
 import app.organicmaps.R;
 import app.organicmaps.util.Graphics;
 import app.organicmaps.util.Config;
@@ -72,8 +75,9 @@ class TabAdapter extends FragmentPagerAdapter
     public void onPageSelected(int position)
     {
       super.onPageSelected(position);
-      if (mTabSelectedListener != null)
+      if (mTabSelectedListener != null) {
         mTabSelectedListener.onTabSelected(Tab.values()[position]);
+      }
     }
   }
 
@@ -91,6 +95,9 @@ class TabAdapter extends FragmentPagerAdapter
     @Override
     public void onTabSelected(TabLayout.Tab tab)
     {
+      SharedPreferences.Editor editor = MwmApplication.prefs(mContext).edit();
+      editor.putInt("lastSelectedTab", tab.getPosition());
+      editor.apply();
       super.onTabSelected(tab);
       Graphics.tint(mContext, tab.getIcon(), androidx.appcompat.R.attr.colorAccent);
     }
@@ -151,7 +158,9 @@ class TabAdapter extends FragmentPagerAdapter
     ViewPager.OnPageChangeListener listener = new PageChangedListener(tabs);
     mPager.addOnPageChangeListener(listener);
     tabs.setOnTabSelectedListener(new OnTabSelectedListenerForViewPager(mPager));
-    listener.onPageSelected(0);
+    SharedPreferences preferences = MwmApplication.prefs(mPager.getContext());
+    int lastSelectedTabPosition = preferences.getInt("lastSelectedTab", 0);
+    listener.onPageSelected(lastSelectedTabPosition);
   }
 
   void setTabSelectedListener(OnTabSelectedListener listener)
